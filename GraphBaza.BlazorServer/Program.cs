ba1c0a189e6b37bf656d744ab90864dcf8d85cb5
sat-lino-au <alberto.gruning.zen@gmai.com>
2025-03-01 15:24:53 +0000

    feat: set program for signalr

--- GraphBaza.BlazorServer/Program.cs
+++ GraphBaza.BlazorServer/Program.cs
@@ -5,14 +5,19 @@ using GraphBaza.Core.Queries;
 using HotChocolate.AspNetCore;
 using Microsoft.AspNetCore.Components.Endpoints;
 using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddRazorComponents()
     .AddInteractiveServerComponents()
-
     ;
+builder.Services.Configure<ForwardedHeadersOptions>(options => {
+    options.ForwardedHeaders = 
+        ForwardedHeaders.XForwardedFor | 
+        ForwardedHeaders.XForwardedProto;
+});
 
 builder.Services
     .AddGraphQLServer()
@@ -20,31 +25,34 @@ builder.Services
 
 var app = builder.Build();
 
+app.UseForwardedHeaders();
 //app.UseHttpsRedirection();
-//app.MapStaticAssets();
 app.UseStaticFiles();
-app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 app.UseRouting();
 app.UseAntiforgery();
 
-app.UseEndpoints(endpoints =>
-{
+//app.UseEndpoints(endpoints =>{
 
-    endpoints.MapRazorComponents<App>()
-        .AddInteractiveServerRenderMode();
+    //endpoints.MapRazorComponents<App>().AddInteractiveServerRenderMode();
     
-    endpoints.MapGraphQL();
+    //endpoints.MapGraphQL();
     
     //endpoints.MapGet("/api", async context =>
     //{	await context.Response.WriteAsync("Say Hello to QiMono !!! \n Use /graphql for API endpoint");});
-});
+//});
 
 if (!app.Environment.IsDevelopment())
 {
 
     app.UseDeveloperExceptionPage();
-    //app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
 }
+app.MapBlazorHub();
+app.MapGraphQL();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+//.AddAdditionalAssemblies(typeof(GraphBaza.BlazorServer.Components.Pages.Home).Assembly)
+    ;
 
 app.Run();
