1b435fb4bff7e4f3233ed59d4c42c91e5e6154de
sat-lino-au <alberto.gruning.zen@gmai.com>
2025-03-04 15:40:08 +0000

    ref: updtated mamespaces for maui hybrid project

---
+++ GraphBaza.MAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphBaza.MAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ GraphBaza.MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿
+namespace GraphBaza.MAUI;
+
+public partial class MainPage : TabbedPage//ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
